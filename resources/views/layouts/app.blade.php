38b0b837758183c15d1a3f960c0d807c2e4cb743
Nugee <nugrohorahmanto24@gmail.com>
2025-12-22 18:45:47 +0700

    final adjustment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>@yield('title', 'Puskesmas')</title>
+    <link rel="icon" type="image/jpeg" href="{{ asset('images/logo.jpeg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
